150334301722cc22a5d330a9982f673fc076c72c
Marcin Kunert <the.martines@gmail.com>
2017-11-27 16:30:04 +0100

    Init

---
+++ src/main/java/pl/javastart/springdataexample/SpringdataexampleApplication.java
@@ -0,0 +1,12 @@
+package pl.javastart.springdataexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdataexampleApplication.class, args);
+	}
+}
